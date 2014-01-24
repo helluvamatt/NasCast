14df013b6d3608534c5ee27e3f9c30d41efae395
Matt Schneeberger <helluvamatt@gmail.com>
2014-01-24 16:18:37 -0600

    Initial commit - project layout, some existing web code from a previous project
    Signed-off-by: Matt Schneeberger <helluvamatt@gmail.com>

---
+++ NasCast.Core/README.md
@@ -0,0 +1,3 @@
+﻿# NasCast.Core
+
+
---
+++ NasCast.Web/app.js
@@ -0,0 +1,57 @@
+// Includes
+var pg = require('pg');
+var express = require('express');
+var fs = require('fs');
+
+var PostgreStore = require('./connect-postgres.js');
+var PgConfig = require('./pg-config.js');
+
+// configuration loaded in memory
+var config = {};
+
+// load configuration
+fs.readFile('config.json', function(err, data) {
+	
+	// read configuration
+	if (!err && data)
+	{
+		config = JSON.parse(data);
+	}
+	config.secret = config.secret || process.env.SECRET;
+	config.database = new PgConfig(config.database || { "global": { url: process.env.DATABASE_URL } });
+	var sessionDatabase = config.database.get('sessions');
+
+	// Configure Express application
+	var app = express();
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use('/assets', express.static(__dirname + '/assets'));
+	app.use('/chromecast', express.static(__dirname + '/chromecast'));
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret: config.secret,
+		store: new PostgreStore(sessionDatabase)
+	}));
+
+	app.get('/', function(req, res) {
+		// TODO Web application home page
+		res.send('hello world');
+	});
+
+	app.get('/movies', function(req, req) {
+		// TODO List Movies in database
+	});
+
+	app.get('/tv', function(req, res) {
+		// TODO List TV shows in database
+	});
+
+	app.get('/stream/:contentId', function(req, res) {
+		// TODO Stream content
+	});
+
+	app.listen(3000);
+	console.log('Server listening on port 3000.');
+
+}); // end fs.readFile callback
+
